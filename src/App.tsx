1b97bd41f0c99249a0a2ba00574d3c1ae6bd23d1
mustafa balci <mbalci85@gmail.com>
2023-12-18 10:37:30 +0000

    app tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Header from './components/Header/Header';
 import MediaQueryContextProvider from './contexts/MediaQueryContextProvider';
