34330e21a6d3d91079ee45a998f259e905e3daeb
mustafa balci <mbalci85@gmail.com>
2023-12-26 09:22:38 +0000

    footer style updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,24 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { MediaQueryContext } from '../../contexts/MediaQueryContextProvider';
 
 const Footer = () => {
+	const { isSmallScreen } = useContext(MediaQueryContext);
 	return (
 		<Box
 			sx={{
 				display: 'flex',
 				justifyContent: 'space-around',
 				alignItems: 'center',
-				height: '5rem',
+				height: isSmallScreen ? '3.5rem' : '5rem',
 				backgroundColor: 'black',
 				color: 'white',
 			}}>
-			<Box>2024 @ CineTrail</Box>
+			<Box>
+				<Typography variant='inherit' sx={{ fontSize: isSmallScreen ? '0.8rem' : '1rem' }}>
+					2024 @ CineTrail
+				</Typography>
+			</Box>
 			<Box>
 				<Button
 					sx={{
@@ -20,11 +27,16 @@ const Footer = () => {
 						border: '0.01rem solid white',
 						padding: '0.2rem',
 						width: '5rem',
-					}}>
+					}}
+					size='small'>
 					About
 				</Button>
 			</Box>
-			<Box>All rights reserved</Box>
+			<Box>
+				<Typography variant='inherit' sx={{ fontSize: isSmallScreen ? '0.8rem' : '1rem' }}>
+					All rights reserved
+				</Typography>
+			</Box>
 		</Box>
 	);
 };
